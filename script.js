9a336709106f2b24ee9e0b17538682005fc8752e
ID <izaakduncan@outlook.com>
2022-06-27 05:22:59 -0700

    initial

---
+++ script.js
@@ -0,0 +1,49 @@
+const timeDisplay = document.getElementById('timeDisplay')
+const timeSelect = document.getElementById('timeSelect')
+const alarmStatus = document.getElementById('alarmStatus')
+const alarmStop = document.getElementById('alarmStop')
+
+let alarmSound = new Audio('alarmSound.mp3')
+
+let userAlarmTime
+
+timeSelect.addEventListener('change', function() {
+    userAlarmTime = timeSelect.value
+    alarmStatus.innerHTML = `Set for ${userAlarmTime}`
+})
+
+let currentTimeCheck
+
+setInterval(function() {
+    let date = new Date()
+    hours = date.getHours()
+    if (date.getMinutes() < 10) {
+        minutes = `0${date.getMinutes()}`
+    } else {
+        minutes = date.getMinutes()
+    }
+    if (date.getSeconds() < 10){
+        seconds = `0${date.getSeconds()}`
+    } else {
+        seconds = date.getSeconds()
+    }
+    timeDisplay.innerHTML = `${hours}:${minutes}:${seconds}`
+
+    currentTimeCheck = `0${hours}:${minutes}`
+
+    if (currentTimeCheck == userAlarmTime) {
+        alarmSound.load()
+        alarmSound.play()
+        alarmSound.loop = true
+        userAlarmTime = 0
+        alarmStatus.innerHTML = 'Alarm Activated!'
+        alarmStop.disabled = false
+        alarmStop.addEventListener('click', function() {
+            alarmSound.pause()
+            alarmStatus.innerHTML = 'not yet set'
+            userAlarmTime = ''
+            alarmStop.disabled = true
+            timeSelect.value = ''
+        })
+    }
+}, 1000)
